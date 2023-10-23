bc79b4f3ce559ed253735fa605ba203e1182bacb
Mandala <premadityamandala@gmail.com>
2023-10-23 17:32:57 +0800

    feat: add swagger

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
 import { Elysia, t } from "elysia";
 import { addData, getData, getDetail } from "./controller/vtuber.controller";
-import { vtuberDTO } from "./models/vtuber.model";
+import { vtuberDTO, vtubersDTO } from "./models/vtuber.model";
+import { swagger } from "@elysiajs/swagger";
 
 export const app = new Elysia()
 
 // Route
-app.group('vtubers', app => app
+app.use(swagger()).group('vtubers', app => app
   .get('/', () => getData())
   .get('/:id', ({params}) => getDetail(+params.id))
-  .post('/', ({body}) => addData(body),{body: vtuberDTO} )
+  .post('/', ({body}) => addData(body),{body: vtuberDTO, response: vtubersDTO, schema: {
+    responses: t.Array(
+      t.Object({
+        id: t.Number(),
+        name: t.String()
+      })
+    )
+  }})
 )
 
 // Run App
